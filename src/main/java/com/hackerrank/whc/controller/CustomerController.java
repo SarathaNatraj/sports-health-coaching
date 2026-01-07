a2157fde9b1d7d9c3fe5059004f6f8042470c0d0
SarathaNatraj <102175569+SarathaNatraj@users.noreply.github.com>
2026-01-07 12:50:00 +0530

    updated customer controller.java

--- src/main/java/com/hackerrank/whc/controller/CustomerController.java
+++ src/main/java/com/hackerrank/whc/controller/CustomerController.java
@@ -1,12 +1,44 @@
 package com.hackerrank.whc.controller;
 
+import com.hackerrank.whc.model.Customer;
 import com.hackerrank.whc.repository.CustomerRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class CustomerController {
 
     final CustomerRepository customerRepository;
 
     public CustomerController(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
+    }
+
+    @PostMapping("/api/customer")
+    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) {
+        Customer saved = customerRepository.save(customer);
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/customer")
+    public ResponseEntity<List<Customer>> getCustomers() {
+        List<Customer> customers = customerRepository.findAll(Sort.by("id"));
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/customer/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Integer id) {
+        Optional<Customer> customer = customerRepository.findById(id);
+        return customer.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
